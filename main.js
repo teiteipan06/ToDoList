26ce64e0db9ea14f79152229e5dd0bdcb654b29f
teiteipan06 <y7321543@gmail.com>
2023-02-21 23:49:37 +0900

    update index.html

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+function getItems() {
+  const value = localStorage.getItem("todo");
+
+  console.log(value);
+}
+
+getItems();
